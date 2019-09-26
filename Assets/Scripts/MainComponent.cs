a9093f1eee311851b5845a2723a41016833a4b8f
Zaski <zaski123@gmail.com>
2019-09-27 01:10:03 +0200

    Changed messages of game

--- Assets/Scripts/MainComponent.cs
+++ Assets/Scripts/MainComponent.cs
@@ -10,30 +10,29 @@ public class MainComponent : MonoBehaviour
 
     private void Start             ()
     {
-        Debug.Log("[DEBUG] Start...");
         
         // CREAMOS AL JUGADOR
-        Weapon weapon = new Weapon ("Stick",       10);
-        Shield shield = new Shield ("Wood Shield", 10);
+        Weapon weapon = new Weapon ("A spoon.",       10);
+        Shield shield = new Shield ("Frying pan", 10);
         m_player = new Player (20, 5, 100, 100, 20, weapon, shield);
 
         // CREAMOS EL LABERINTO QUE CONTIENE LAS HABITACIONES
         m_labyrinth = new Labyrinth(m_player);
 
         // SE AÑADEN LAS HABITACIONES
-        Enemy enemy = new Enemy  (m_player, "Angry Ent", 5, 5, 120, 60, 10, 10, 1, 20);
+        Enemy enemy = new Enemy  (m_player, "Rat kid", 5, 5, 120, 60, 10, 10, 1, 20);
         Room  room  = new Room(Room.RoomType.ENEMY, enemy);
         m_labyrinth.AddRoom(room);
 
-        Item item = new Item("J+RB", -5, 10, 10, 10, 0, -5);
+        Item item = new Item("Dark Matter", -5, 10, 10, 10, 0, -5);
         room = new Room(Room.RoomType.ITEM, item);
         m_labyrinth.AddRoom(room);
 
-        Trap trap = new Trap("Examen", -10, -10);
+        Trap trap = new Trap("Existential crysis", -10, -10);
         room = new Room(Room.RoomType.TRAP, trap);
         m_labyrinth.AddRoom(room);
 
-        enemy = new Enemy  (m_player, "30fps", 5, 5, 120, 60, 10, 10, 1, 20);
+        enemy = new Enemy  (m_player, "GabeN", 5, 5, 120, 60, 10, 10, 1, 20);
         room = new Room(Room.RoomType.ENEMY, enemy);
         m_labyrinth.AddRoom(room);
 
@@ -59,7 +58,7 @@ public class MainComponent : MonoBehaviour
 
                 if (m_labyrinth.GetCurrentRoom().enemy.IsDead())
                 {
-                    Debug.Log("No pudiste aguantar los meteoros caballero de hojalata");
+                    Debug.Log("You won this battle!");
                     m_labyrinth.ChangeRoom (); 
                 }
             }
